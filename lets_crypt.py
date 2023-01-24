8faca3922736b1758739054e31a59590c7ca2593
RyoDeyn <jaimebienlepain@gmail.com>
2023-01-24 23:28:53 +0100

    Initialization of the script.

---
+++ lets_crypt.py
@@ -0,0 +1,36 @@
+"""A python script that allows you to encrypt your personal files.
+
+Usage:
+======
+    python lets_crypt.py [OPTIONS]
+
+    OPTIONS:
+    (TO DEFINE)
+
+License:
+======
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""
+
+__author__ = "Alexandre Quéré aka Ryo Deyn"
+__version__ = "v1.0.0-alpha"
+__license__ = "MPL-2.0 license"
+__copyright__ = "Copyright (C) 2023 Alexandre Quéré"
+
+import sys
+import getopt
+
+# Potential class :
+
+# Potential global variables :
+
+
+def main():
+    """Main function."""
+    # process_options()
+
+
+if __name__ == '__main__':
+    main()
